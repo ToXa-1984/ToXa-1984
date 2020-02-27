8cc0bc7e60dbd4f16789172e3fe4e5b6811ae691
ToXa-1984 <anton1984kostiuchenko@gmail.com>
2020-02-27 12:14:42 +0200

    27.02.2020

--- src/main/java/lesson12homework/Main.java
+++ src/main/java/lesson12homework/Main.java
@@ -8,10 +8,10 @@ public class Main {
 
     public static void main(String[] args) {
 
-//        ArrayList list = new ArrayList();
-//        list = FirstMainMethod(list);
-//        list = SecondMainMethod(list);
-//        ThirdMainMethod(list);
+        ArrayList list = new ArrayList();
+        list = FirstMainMethod(list);
+        list = SecondMainMethod(list);
+        ThirdMainMethod(list);
 
         HashSet hashSet = new HashSet();
         hashSet = FirstHashMethod(hashSet);
